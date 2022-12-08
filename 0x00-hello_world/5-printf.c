c59b963ffe17f339f4e32808b5dc0d084a4466e4
lydiawayua <lydiawkyalo@gmail.com>
2022-12-08 07:41:03 -0800

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ *  main - Entry point
+ *  
+ *   Returns: Always 0 (success)
+ */
+int main(void)
+{
+	pointf("with proper grammar,but the outcome is a piece of art,\n");
+	Return (0);
+}
+
