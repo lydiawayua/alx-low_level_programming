19f7594b46eccfe46381c03bd3b669f860c0eb7b
lydiawayua <lydiawkyalo@gmail.com>
2022-12-08 08:59:46 -0800

    hello world

---
+++ 6-size.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+	char c;
+	int d;
+	long int 1d;
+	long long int 11d
+	float f;
+
+printf("Size of a char: %lu bytes(s)\n", (unsingned long)sizeof(c));
+printf("Size of an int: %lu bytes(s)\n",(unsigned long)sizeof(d));
+printf("Size of long int: %lu bytes(s)\n",(unsigned long)sizeof(1d);
+printf("Size of long long int: %lu bytes(s)\n",(unsigned  long)sizeof(11d));
+printf("Size of a float: %lu bytes(s)\n",(unsigned long)sizeof(f));
+
+   return (0);
+}
+
+
