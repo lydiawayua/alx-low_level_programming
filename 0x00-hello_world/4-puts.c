1613a86f743c58d12ea8b489c95d0627eadf01aa
lydiawayua <lydiawkyalo@gmail.com>
2022-12-08 07:25:03 -0800

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0(success)
+ */
+int  main(void)
+{
+	 char test_str[] = "/"Programming is like building a multilingual puzzle";
+
+	  puts(test_str);
+	  Return(0);
+}
