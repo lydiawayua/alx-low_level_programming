ac778db0fde0ac690aabba4991f65db6e74071d6
lydiawayua <lydiawkyalo@gmail.com>
2022-12-09 07:19:02 -0800

    alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,18 +8,14 @@
 int main(void)
 {
 	char c = 'a';
-	char C = 'A';
 
 	while (c <= 'z')
 	{
-		putchar(c);
+		if (c != 'q' && c != 'e')
+			putchar(c);
+
 		c++;
-	}
-	while (C <= 'Z')
-	{
-		putchar(C);
-		C++;
-	}
+	}	
 	putchar('\n');
 
 	return (0);
