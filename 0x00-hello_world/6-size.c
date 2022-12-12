bdffce606e1afdb831835b6e36f89dd7b12a7caa
lydiawayua <lydiawkyalo@gmail.com>
2022-12-12 07:09:24 -0800

    size of integer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,49 +3,18 @@
 /**
  * main - Entry point
  *
-<<<<<<< HEAD
- *  Return: Always 0 (Success)
+ * main - prints the size of various data types that it is compiled and run on
+ *
+ * Return: Always 0 (Success)
  */
-int main(void
-{
-    char c:
-    int d:
-    long int 1d;
-    long long int 11d;
-    float f;
 
-    printf("Size of char:%1u byte(s)\n", (unsigned long)sizeof(c));
-    printf("Size of an int:%1ubyte(s)\n",(unsigned long)sizeof(d));
-    printf("Size of a long int:%lubyte(s)\n",(unsigned long)sizeof(d));
-    printf("Size of long int:%lu bytes(s)\n",(unsigned long)sizeof(11d));
-    printf("Size of float:%lu bytes(s)\n",(unsigned long)sizeof(f));
-
-    return (0);
-    
- }
-=======
- *
- *
- *
- *
- *
- * Return: 0 (Success)
- */
 int main(void)
 {
-
-	char c;
-	int d;
-	long int ld;
-	long long int lld;
-	float f;
-
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(ld));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
-	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
->>>>>>> d6e909d9fcdf7be2b1ef33ba983da3ca130f417d
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-script that runs a C file through the preprocessor and save the result into another file
+Size is not grandeur,and territory does not make a nation
