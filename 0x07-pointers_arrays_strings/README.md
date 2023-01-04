1dafbe4ff8adaff50c888546c64c4d3ddb6edadd
lydiawayua <lydiawkyalo@gmail.com>
2023-01-04 06:57:57 -0800

    memcpy

--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,1 +1,1 @@
-function that fills memory with a costant byte
+function that copies memory area
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio:h>
+#include <stdio.h>
 
 char *_memset(char *s, char b, unsighned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
